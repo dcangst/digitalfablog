f291b842f5e75666e523ad516b124780b3011606
Daniel Angst <dcangst@gmail.com>
2018-11-18 01:32:42 +0100

    update help field for Booking - account

--- cashier/models.py
+++ cashier/models.py
@@ -108,7 +108,7 @@ class Booking(models.Model):
     account = models.TextField(
         max_length=4,
         verbose_name=_("account"),
-        help_text=_("account"))
+        help_text=_("account to make the booking to/from"))
 
     timestamp = models.DateTimeField(
         auto_now_add=True,
